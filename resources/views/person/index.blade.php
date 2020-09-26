6ed54504458592f9f00f649557247cc9313b5256
Naoki Kanatani <naokiokane0616@icloud.com>
2020-09-26 10:14:12 +0900

    third commit

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -1,8 +1,31 @@
 @extends('person/layout')
 @section('content')
 <body>
-  <div class="container">
-  <h1>good</h1>
+  <div>
+    <div class="header">
+    <div class="back-font">
+      <h1>いいね</h1>
+      <nav class=top_nav>
+        <ul>
+          <li>
+            <a href="">top</a>
+          </li>
+          <li>
+            <a href="">sign up</a>
+          </li>
+          <li>
+            <a href="">お問い合わせ</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    
+    
+    </div>
+    <div>
+    </div>
+
+  <h1>性格分析</h1>
     <ul>
       <li><a href="">title</a></li>
       <li><a href="">title</a></li>
