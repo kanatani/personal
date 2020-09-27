6cfacab74d3b3bda5abdabacfdbd5e503e43186f
Naoki Kanatani <naokiokane0616@icloud.com>
2020-09-27 12:03:19 +0900

    link delete

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -1,44 +1,46 @@
 @extends('person/layout')
 @section('content')
 <body>
-  <div>
-    <div class="header">
-    <div class="back-font">
-      <h1>person</h1>
-      <nav class=top_nav>
-        <ul>
-          <li>
-            <a href="">top</a>
-          </li>
-          <li>
-            <a href="">sign up</a>
-          </li>
-          <li>
-            <a href="">お問い合わせ</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-    </div>
-    <div class="top_intro">
-      <h1 class="intro_title">無料診断テスト</h1>
-      <div class="boxes">
-        <div class="box">
-        <h3>性格を診断</h3>
-          <i class="fas fa-user-friends"></i>
-          <p>big5からなる性格診断で5つの観点から性格をみることができる</p>
-        </div>
-        <div class="box">
-          <h3>相手の性格診断</h3>
-          <i class="fas fa-user-circle"></i>
-          <p>気になっている相手の性格をみて分析してみよう</p>
-        </div>
-        
-        <div class="box">
-        <h3>見つけよう</h3>
-        <i class="fas fa-male"></i>
-        <p>掲示板機能やグループチャットで気になる人を見つけよう</p>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="header col-xs-12">
+      <div class="back-font">
+        <h1>person</h1>
+        <nav class=top_nav>
+          <ul>
+            <li>
+              <a href="">top</a>
+            </li>
+            <li>
+              <a href="">sign up</a>
+            </li>
+            <li>
+              <a href="">お問い合わせ</a>
+            </li>
+          </ul>
+        </nav>
       </div>
+      </div>
+      </div>
+      <div class="top_intro">
+        <h1 class="intro_title">無料診断テスト</h1>
+        <div class="boxes">
+          <div class="box">
+          <h3>性格を診断</h3>
+            <i class="fas fa-user-friends"></i>
+            <p>big5からなる性格診断で5つの観点から性格をみることができる</p>
+          </div>
+          <div class="box">
+            <h3>相手の性格診断</h3>
+            <i class="fas fa-user-circle"></i>
+            <p>気になっている相手の性格をみて分析してみよう</p>
+          </div>
+          
+          <div class="box">
+          <h3>見つけよう</h3>
+          <i class="fas fa-male"></i>
+          <p>掲示板機能やグループチャットで気になる人を見つけよう</p>
+        </div>
       </div>
     </div>
 
@@ -55,8 +57,8 @@
       <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false"></a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
       <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-width="" data-layout="button" data-action="recommend" data-size="small" data-share="false"></div>
+  
   </div>
-
   <footer>
     <nav class="footer_nav">
     <ul>
