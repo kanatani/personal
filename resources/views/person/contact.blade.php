b7c4e929830ad3b826fb4e3fb98a88531f467b21
Naoki Kanatani <naokiokane0616@icloud.com>
2020-09-28 13:59:34 +0900

    vue add

--- resources/views/person/contact.blade.php
+++ resources/views/person/contact.blade.php
@@ -1,5 +1,19 @@
 @extends('person/layout')
 @section('content')
 <body>
-  ウィッス
+  <div class="container-fluid">
+    <header>
+        <div>
+            <div class="header_title">
+                <h1>person</h1>
+            </div>
+            <div id="app">
+                <div :class="{'is-active: isActive}" class="btn-humburger" @click="toggleButton"></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+        
+    </header>
+  </div>
 @endsection
