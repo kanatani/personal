c82883dfe966dc33409b1365d9a8b76aad55c51b
Naoki Kanatani <naokiokane0616@icloud.com>
2020-12-24 09:52:19 +0900

    community

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Request as PostRequest;
+use Illuminate\Support\Facades\DB;
+
+class TestController extends Controller
+{
+    //
+}
