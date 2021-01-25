1587cd819afe9877fe13346efd7df1ceedfc5a7c
Naoki Kanatani <naokiokane0616@icloud.com>
2021-01-25 15:09:48 +0900

    circleci

--- config/database.php
+++ config/database.php
@@ -56,7 +56,6 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'unix_socket' => '/var/run/mysqld/mysqld.sock',
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
@@ -102,7 +101,6 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'unix_socket' => '/var/run/mysqld/mysqld.sock',
             'strict' => true,
             'engine' => null,
         ],
